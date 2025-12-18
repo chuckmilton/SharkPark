abfcb8596c1b6b2addef4aa14932848dde0f50a4
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2025-12-17 22:18:57 -0800

    fix: CI mobile Jest config for pnpm

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: 'react-native',
+
+  // pnpm hoists packages to the root node_modules/.pnpm folder
+  // We need to transform these ESM packages for Jest
+  transformIgnorePatterns: [
+    '<rootDir>/../../node_modules/(?!\\.pnpm/(' +
+      '@react-native|' +
+      'react-native|' +
+      '@react-navigation' +
+      ').*)',
+  ],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
 };
